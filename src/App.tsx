f95e2247258826e107425e23c13881e1a3ea94b6
Emma Oyetey <emmaflookis@hotmail.com>
2024-09-18 14:47:29 +0100

    all set upfiles reviewed and checked for errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TestComponent from './components/testComponent' // Import the TestComponent
-
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TestComponent />} /> {/* Route to the TestComponent */}
-      </Routes>
-    </Router>
+    <div>
+      <h1>Emma Oyetey</h1>
+    </div>
   );
-};
+}
 
 export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Test from './components/Test';
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Test />} /> {/* Route to the TestComponent */}
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
