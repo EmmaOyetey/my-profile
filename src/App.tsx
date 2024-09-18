034608a9a4a1e82738f16b0c8dfeaca24cd21a3d
Emma Oyetey <emmaflookis@hotmail.com>
2024-09-18 15:05:29 +0100

    created basic landing page Home.tsx and added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-function App() {
-  return (
-    <div>
-      <h1>Emma Oyetey</h1>
-    </div>
-  );
-}
-
-export default App;
-
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Test from './components/Test';
-
-// const App: React.FC = () => {
+// function App() {
 //   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Test />} /> {/* Route to the TestComponent */}
-//       </Routes>
-//     </Router>
+//     <div>
+//       <h1>Emma Oyetey</h1>
+//     </div>
 //   );
-// };
+// }
 
 // export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Route to the TestComponent */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import "./Home.scss";
+
+const Home: React.FC = () => {
+    return (
+      <div className = "home">
+        <h1 className = "home_name">Emma Oyetey</h1>
+      </div>
+    );
+  };
+  
+  export default Home;
