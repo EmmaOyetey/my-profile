386452ff1d6e4c88d6544ec67f2f5d4e9d86acb0
Emma Oyetey <emmaflookis@hotmail.com>
2024-09-19 12:51:15 +0100

    deployment not working simplified code to attempt to resolve error

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,41 +1,40 @@
-// import "./Home.scss";
-// import LandingContent from "../components/LandingContent/LandingContent";
-
-// const Home: React.FC = () => {
-//     return (
-//       <div className = "">
-        
-//       </div>
-//     );
-//   };
-  
-//   export default Home;
-
-import React, { useEffect, useState } from "react";
-import LandingPage from '../components/LandingContent/LandingContent'; // Adjust the path as needed
-import AboutMe from '../components/AboutMe/AboutMe'; // Import the new AboutMe component
 import "./Home.scss";
 
-const Home: React.FC = () => {
-    const [showLanding, setShowLanding] = useState(true);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowLanding(false);
-        }, 3000); // 30 seconds
-
-        return () => clearTimeout(timer); // Cleanup the timer on unmount
-    }, []);
-
+const LandingContent: React.FC = () => {
     return (
-        <div className="home">
-            {showLanding ? (
-                <LandingPage />
-            ) : (
-                <AboutMe /> // Replace with the new AboutMe component
-            )}
-        </div>
+      <div className = "home">
+        <h1 className = "name">Emma Oyetey</h1>
+      </div>
     );
-};
+  };
+  
+  export default LandingContent;
 
-export default Home;  
+// import React, { useEffect, useState } from "react";
+// import LandingPage from '../components/LandingContent/LandingContent'; // Adjust the path as needed
+// import AboutMe from '../components/AboutMe/AboutMe'; // Import the new AboutMe component
+// import "./Home.scss";
+
+// const Home: React.FC = () => {
+//     const [showLanding, setShowLanding] = useState(true);
+
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
+//             setShowLanding(false);
+//         }, 3000); // 30 seconds
+
+//         return () => clearTimeout(timer); // Cleanup the timer on unmount
+//     }, []);
+
+//     return (
+//         <div className="home">
+//             {showLanding ? (
+//                 <LandingPage />
+//             ) : (
+//                 <AboutMe /> // Replace with the new AboutMe component
+//             )}
+//         </div>
+//     );
+// };
+
+// export default Home;  
